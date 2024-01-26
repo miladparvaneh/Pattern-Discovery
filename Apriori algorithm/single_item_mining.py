2edb4990334ab0cb2e01354fab7e886d4a49bbd6
miladparvaneh <64170986+miladparvaneh@users.noreply.github.com>
2024-01-26 16:54:38 -0600

    Update single_item_mining.py

--- Apriori algorithm/single_item_mining.py
+++ Apriori algorithm/single_item_mining.py
@@ -1,1 +1,24 @@
+# Setting thresholds
+rel_min_sup = 0.01
+abs_min_sup = 771
 
+# Initializing single item dictionary
+single_item_dict = {}
+
+# Reading the input file and mining frequent single items
+with open('input_file.txt', 'r') as input:
+    for line in input:
+        items = line.strip().split(';')
+        for item in items:
+            if item in single_item_dict:
+                single_item_dict[item] += 1
+            else:
+                single_item_dict[item] = 1
+
+# Filtering by minimum support threshold
+frequent_single_item_dict = {key: value for key, value in single_item_dict.items() if value > abs_min_sup}
+
+# Writing the frequent single items
+with open('patterns_single.txt', 'w') as output_single:
+    for key, value in frequent_single_item_dict.items():
+        output_single.write(f'{value}:{key}\n')
